57b4dc5788a9852fc256a6fdc4ae3bb0fa316ca7
juanjose <juanjose.cabral.vega.alu@iesfernandoaguilar.es>
2021-02-25 08:18:08 +0100

    Cadena

---
+++ Cadena.java
@@ -0,0 +1,8 @@
+import java.util.*;
+	public class Cadena {
+		public static void main(String[] args){
+			Scanner sc=new Scanner(System.in);
+			
+			System.out.println("dame una cadena");
+			
+			}}
